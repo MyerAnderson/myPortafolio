83b4348c006a26130f78591c1292edfa0b1cf9e6
andersonMyer <93794229+MyerAnderson@users.noreply.github.com>
2023-10-06 19:12:37 -0500

    finished, jus a few tounches lef

--- vistas/main.js
+++ vistas/main.js
@@ -87,3 +87,85 @@ ham.addEventListener("click",()=>{
         document.body.style.overflowY = "auto"
     })
 })
+
+//btn
+
+const btnSobreMi = document.querySelector(".btnSobreMi");
+
+btnSobreMi.addEventListener("click",()=>{
+    window.location.href ="#sobreMi";
+})
+
+//FORMULARIO
+
+const inputs = document.querySelectorAll(".formulario input");
+const formulario = document.querySelector(".formulario");
+const btn = document.querySelector(".formulario .btn");
+
+const expresiones = {
+	nombres: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+const campos = {
+	nombres: false,
+	telefono: false,
+	email: false
+}
+
+
+
+function verificar() {
+    if (campos.nombres && campos.telefono && campos.email === true){
+        btn.classList.remove("inhabilitado");
+        btn.classList.add("btnEnviar");
+    }
+    else{
+        btn.classList.remove("btnEnviar");
+        btn.classList.add("inhabilitado");
+    }
+}
+
+function validar(e){
+    switch(e.target.name){
+        case "nombres" :
+            const name = validarCampo(expresiones.nombres,e.target.value);
+            campos.nombres = name;
+        break;
+        case "telefono" :
+            const tel = validarCampo(expresiones.telefono,e.target.value);
+            campos.telefono = tel;
+        break;
+        case "email" :
+            const email = validarCampo(expresiones.email,e.target.value);
+            campos.email = email;
+        break;
+    }
+    verificar();
+    
+}
+
+function validarCampo(exp,valor){
+    return exp.test(valor);
+}
+
+inputs.forEach(input => {
+    input.addEventListener("keyup",validar);
+    input.addEventListener("blur",validar);
+    
+});
+
+formulario.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const btn = document.querySelector(".formulario .btnEnviar");
+    if (btn){
+        formulario.reset();
+        campos.nombres = false;
+        campos.telefono = false;
+        campos.email = false;
+        btn.classList.remove("btnEnviar");
+        btn.classList.add("inhabilitado");
+    }
+
+})
